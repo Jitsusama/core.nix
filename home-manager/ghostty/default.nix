7bb98fae0642a8830ac3872f2356c2adfe13fe8e
Joel Gerber <joel@grrbrr.ca>
2025-10-07 22:18:21 -0400

    feat: add wallpaper support to Ghostty config (#43)
    - Updated Ghostty config to include background image settings
    - Added WALLPAPER_PATH placeholder that gets replaced at build time
    - Module now uses baroque-ornate-charcoal.png from wallpapers flake
    - Maintains all settings in static config file except the dynamic path

--- home-manager/ghostty/default.nix
+++ home-manager/ghostty/default.nix
@@ -1,6 +1,7 @@
-{ ... }:
+{ lib, wallpapers, ... }:
 {
-  xdg.configFile = {
-    "ghostty/config".source = ./config;
-  };
+  xdg.configFile."ghostty/config".text = builtins.replaceStrings
+    [ "WALLPAPER_PATH" ]
+    [ "${wallpapers.tiles}/baroque-ornate-charcoal.png" ]
+    (builtins.readFile ./config);
 }
