07bacbbed609ff76bfc6697a825932fa63d0de9d
Joel Gerber <joel.gerber@shopify.com>
2025-10-07 21:59:13 -0400

    feat: add wallpapers flake input and output
    Adds wallpapers.nix as an input and re-exports it as an output,
    allowing downstream flakes to access the wallpaper collection
    through core.wallpapers.

--- flake.nix
+++ flake.nix
@@ -1,11 +1,16 @@
 {
   description = "Opinionated Core Nix Configuration";
 
+  inputs = {
+    wallpapers.url = "github:Jitsusama/wallpapers.nix";
+  };
+
   outputs =
-    { ... }:
+    { wallpapers, ... }:
     {
       nix-darwin = ./nix-darwin;
       home-manager = ./home-manager;
+      wallpapers = wallpapers;
     };
 }
 
